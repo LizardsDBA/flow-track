b0a8bd811f72f37f9b18bf2e5604b90d9ae388a8
Lucas Castro <lucascastrosantos20007@gmail.com>
2026-03-20 22:29:39 -0300

    chore<untracked>: Estruturação de pastas do projeto

--- src/main/java/br/edu/fatec/api/flowtrack/FlowtrackApplication.java
+++ src/main/java/br/edu/fatec/api/flowtrack/FlowtrackApplication.java
@@ -1,3 +1,4 @@
+
 package br.edu.fatec.api.flowtrack;
 
 import org.springframework.boot.SpringApplication;
@@ -5,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowtrackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlowtrackApplication.class, args);
 	}
-
 }
+
