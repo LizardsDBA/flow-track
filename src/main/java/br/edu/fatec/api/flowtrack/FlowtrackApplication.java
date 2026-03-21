a26e4424fcd120be23f220f4e9bed0dcefb37d12
Lucas Castro <lucascastrosantos20007@gmail.com>
2026-03-20 21:23:26 -0300

    feat<untracked>: preparação do projeto base, primeiro commit

---
+++ src/main/java/br/edu/fatec/api/flowtrack/FlowtrackApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.api.flowtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fatec/api/flowtrack/FlowtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.api.flowtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
